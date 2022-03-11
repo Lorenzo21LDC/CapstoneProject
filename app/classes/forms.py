bf9c9bbfc6e8ebc03cbf0a0a6fa355d4b0708312
lorenzodrilleaucraft <s_lorenzo.drilleaucraft@ousd.org>
2022-03-11 13:12:07 -0800

    first commit

--- app/classes/forms.py
+++ app/classes/forms.py
@@ -21,6 +21,8 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])  
+    fname = StringField('Fname', validators=[DataRequired()])
+    lname = StringField('Lname', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
@@ -58,6 +60,7 @@ class ProfileForm(FlaskForm):
     lname = StringField('Last Name', validators=[DataRequired()]) 
     image = FileField("Image") 
     submit = SubmitField('Post')
+    role = SelectField('Role',choices=[("Teacher","Teacher"),("Student","Student")])
 
 class PostForm(FlaskForm):
     subject = StringField('Subject', validators=[DataRequired()])
